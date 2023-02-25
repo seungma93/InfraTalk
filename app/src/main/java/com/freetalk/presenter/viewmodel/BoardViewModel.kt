15c68e56450d9ecdf9dd962b9f4f0ad320d3fcb0
seungma <seungma93@naver.com>
2023-02-25 15:41:55 +0900

    인덴트 수정

--- app/src/main/java/com/freetalk/presenter/viewmodel/BoardViewModel.kt
+++ app/src/main/java/com/freetalk/presenter/viewmodel/BoardViewModel.kt
@@ -9,11 +9,11 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 
-sealed class BoardViewEvent{
-    data class Insert(val respond: Respond): BoardViewEvent()
+sealed class BoardViewEvent {
+    data class Insert(val respond: Respond) : BoardViewEvent()
 }
 
-class BoardViewModel(private val useCase: BoardUseCase): ViewModel() {
+class BoardViewModel(private val useCase: BoardUseCase) : ViewModel() {
     private val _viewEvent = MutableSharedFlow<BoardViewEvent>()
     val viewEvent: SharedFlow<BoardViewEvent> = _viewEvent.asSharedFlow()
 
