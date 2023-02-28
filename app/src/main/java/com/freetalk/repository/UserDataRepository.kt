1c56aeaa267b9d8f91c943bef300e95effe73cb6
seungma <seungma93@naver.com>
2023-02-28 17:38:57 +0900

    User / 로그인 로직 수정 + 싱글톤에 회원 정보 저장

--- app/src/main/java/com/freetalk/repository/UserDataRepository.kt
+++ app/src/main/java/com/freetalk/repository/UserDataRepository.kt
@@ -2,21 +2,22 @@ package com.freetalk.repository
 
 import com.freetalk.data.entity.UserEntity
 import com.freetalk.data.remote.AuthData
+import com.freetalk.data.remote.LoginData
 import com.freetalk.data.remote.UserDataSource
 
 interface UserDataRepository {
     suspend fun signUp(userData: UserEntity): AuthData
-    suspend fun logIn(userData: UserEntity): AuthData
+    suspend fun logIn(userData: UserEntity): LoginData
     suspend fun resetPassword(userData: UserEntity): AuthData
 }
 
-class FirebaseUserDataRepositoryImpl(private val dataSource: UserDataSource<AuthData>): UserDataRepository{
+class FirebaseUserDataRepositoryImpl(private val dataSource: UserDataSource): UserDataRepository{
 
     override suspend fun signUp(userData: UserEntity): AuthData {
         return dataSource.signUp(userData)
     }
 
-    override suspend fun logIn(userData: UserEntity): AuthData {
+    override suspend fun logIn(userData: UserEntity): LoginData {
         return dataSource.logIn(userData)
     }
 
