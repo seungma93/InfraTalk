e6b31c19dfd4b267998e24cdafdea3074b244e29
seungma <seungma93@naver.com>
2023-10-02 17:03:45 +0900

    feature/board/list/printList 보드 로딩 속도 개선

--- app/src/main/java/com/freetalk/data/datasource/remote/FirebaseBoardDataSource.kt
+++ app/src/main/java/com/freetalk/data/datasource/remote/FirebaseBoardDataSource.kt
@@ -21,6 +21,8 @@ import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.QuerySnapshot
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.tasks.await
 import toEntity
 import java.util.Date
@@ -42,21 +44,21 @@ class FirebaseBoardRemoteDataSourceImpl @Inject constructor(
     private var lastDocument: DocumentSnapshot? = null
 
     override suspend fun insertBoard(boardInsertRequest: BoardInsertRequest): BoardInsertResponse {
-            return kotlin.runCatching {
-                val createTime = boardInsertRequest.createTime
-                database.collection("Board")
-                    .add(boardInsertRequest)
-                    .await()
+        return kotlin.runCatching {
+            val createTime = boardInsertRequest.createTime
+            database.collection("Board")
+                .add(boardInsertRequest)
+                .await()
 
-                BoardInsertResponse(
-                    boardAuthorEmail = boardInsertRequest.authorEmail,
-                    boardCreteTime = createTime,
-                    isSuccess = true
-                )
-            }.onFailure {
-                throw FailInsertException("인서트에 실패 했습니다")
-            }.getOrThrow()
-        }
+            BoardInsertResponse(
+                boardAuthorEmail = boardInsertRequest.authorEmail,
+                boardCreteTime = createTime,
+                isSuccess = true
+            )
+        }.onFailure {
+            throw FailInsertException("인서트에 실패 했습니다")
+        }.getOrThrow()
+    }
 
     private suspend fun getBoardDocuments(
         limit: Long,
@@ -71,35 +73,40 @@ class FirebaseBoardRemoteDataSourceImpl @Inject constructor(
         }
     }
 
-    override suspend fun selectBoardMetaList(boardMetaListSelectRequest: BoardMetaListSelectRequest): BoardMetaListResponse {
-        return kotlin.runCatching {
-            val snapshot = when (boardMetaListSelectRequest.reload) {
-                true -> getBoardDocuments(10, null)
-                false -> getBoardDocuments(10, lastDocument)
-            }
-            lastDocument = snapshot.documents.lastOrNull()
+    override suspend fun selectBoardMetaList(boardMetaListSelectRequest: BoardMetaListSelectRequest): BoardMetaListResponse =
+        coroutineScope {
+            kotlin.runCatching {
+                val snapshot = when (boardMetaListSelectRequest.reload) {
+                    true -> getBoardDocuments(10, null)
+                    false -> getBoardDocuments(10, lastDocument)
+                }
+                lastDocument = snapshot.documents.lastOrNull()
 
-            snapshot.documents.map {
-                val authorEmail = it.data?.get("authorEmail")?.let { it as String } ?: error("")
-                BoardMetaResponse(
-                    author = userDataSource
-                        .selectUserInfo(UserSelectRequest(userEmail = authorEmail))
-                        .toEntity(),
-                    title = it.data?.get("title") as? String,
-                    content = it.data?.get("content") as? String,
-                    images = (it.data?.get("images") as? List<String>)?.let {
-                        ImagesResultEntity(it.map { Uri.parse(it) }, emptyList())
-                    },
-                    createTime = (it.data?.get("createTime") as? Timestamp)?.toDate(),
-                    editTime = (it.data?.get("editTime") as? Timestamp)?.toDate()
-                )
-            }.let {
-                BoardMetaListResponse(it)
-            }
-        }.onFailure {
-            throw FailSelectException("셀렉트에 실패 했습니다", it)
-        }.getOrThrow()
-    }
+                snapshot.documents.map {
+                    val authorEmail = it.data?.get("authorEmail")?.let { it as String } ?: error("")
+                    val asyncUserInfo = async {
+                        userDataSource
+                            .selectUserInfo(UserSelectRequest(userEmail = authorEmail))
+                    }
+                    it to asyncUserInfo
+                }.map { (it, deferred) ->
+                    BoardMetaResponse(
+                        author = deferred.await().toEntity(),
+                        title = it.data?.get("title") as? String,
+                        content = it.data?.get("content") as? String,
+                        images = (it.data?.get("images") as? List<String>)?.let {
+                            ImagesResultEntity(it.map { Uri.parse(it) }, emptyList())
+                        },
+                        createTime = (it.data?.get("createTime") as? Timestamp)?.toDate(),
+                        editTime = (it.data?.get("editTime") as? Timestamp)?.toDate()
+                    )
+                }.let {
+                    BoardMetaListResponse(it)
+                }
+            }.onFailure {
+                throw FailSelectException("셀렉트에 실패 했습니다", it)
+            }.getOrThrow()
+        }
 
 
     override suspend fun updateBoard(boardUpdateRequest: BoardUpdateRequest): BoardMetaResponse {
@@ -169,20 +176,20 @@ class FirebaseBoardRemoteDataSourceImpl @Inject constructor(
                     .whereEqualTo("authorEmail", boardAuthorEmail)
                     .whereEqualTo("createTime", boardCreateTime).get().await()
 
-                    snapshot.documents.firstOrNull()?.let {
-                        val authorEmail = it.data?.get("authorEmail")?.let { it as String } ?: error("")
-                        BoardMetaResponse(
-                            author = userDataSource
-                                .selectUserInfo(UserSelectRequest(userEmail = authorEmail))
-                                .toEntity(),
-                            title = it.data?.get("title") as? String,
-                            content = it.data?.get("content") as? String,
-                            images = (it.data?.get("images") as? List<String>)?.let {
-                                ImagesResultEntity(it.map { Uri.parse(it) }, emptyList())
-                            },
-                            createTime = (it.data?.get("createTime") as? Timestamp)?.toDate(),
-                            editTime = (it.data?.get("editTime") as? Timestamp)?.toDate()
-                        )
+                snapshot.documents.firstOrNull()?.let {
+                    val authorEmail = it.data?.get("authorEmail")?.let { it as String } ?: error("")
+                    BoardMetaResponse(
+                        author = userDataSource
+                            .selectUserInfo(UserSelectRequest(userEmail = authorEmail))
+                            .toEntity(),
+                        title = it.data?.get("title") as? String,
+                        content = it.data?.get("content") as? String,
+                        images = (it.data?.get("images") as? List<String>)?.let {
+                            ImagesResultEntity(it.map { Uri.parse(it) }, emptyList())
+                        },
+                        createTime = (it.data?.get("createTime") as? Timestamp)?.toDate(),
+                        editTime = (it.data?.get("editTime") as? Timestamp)?.toDate()
+                    )
                 } ?: run {
                     throw FailSelectBoardContentException("보드 콘텐츠 셀렉트 실패")
                 }
