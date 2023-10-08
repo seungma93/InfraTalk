56207bee8374ac6ad253778990b5ae497deb3504
seungma <seungma93@naver.com>
2023-10-08 21:30:30 +0900

    feature/board/list/printList 채팅방 생성 usecase 까지 구현

---
+++ app/src/main/java/com/freetalk/domain/entity/ChatEntity.kt
@@ -0,0 +1,11 @@
+package com.freetalk.domain.entity
+
+data class ChatRoomCreateEntity(
+    val member: List<String>,
+    val isSuccess: Boolean
+)
+
+data class ChatRoomCheckEntity(
+    val member: List<String>,
+    val isChatRoom: Boolean
+)
---
+++ app/src/main/java/com/freetalk/presenter/form/ChatRoomCheckForm.kt
@@ -0,0 +1,5 @@
+package com.freetalk.presenter.form
+
+data class ChatRoomCheckForm(
+    val member: List<String>
+)
