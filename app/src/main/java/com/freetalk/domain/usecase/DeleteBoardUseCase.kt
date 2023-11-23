51e57b7713d55eb4b8fecbc0f8af5ff475da09f9
seungma <seungma93@naver.com>
2023-11-23 21:30:44 +0900

    feature/board/base 게시글 삭제 UseCase 수정

--- app/src/main/java/com/freetalk/domain/usecase/DeleteBoardUseCase.kt
+++ app/src/main/java/com/freetalk/domain/usecase/DeleteBoardUseCase.kt
@@ -3,10 +3,15 @@ package com.freetalk.domain.usecase
 import com.freetalk.domain.entity.BoardListEntity
 import com.freetalk.domain.repository.BoardDataRepository
 import com.freetalk.domain.repository.BookmarkDataRepository
+import com.freetalk.domain.repository.CommentDataRepository
 import com.freetalk.domain.repository.LikeDataRepository
 import com.freetalk.presenter.form.BoardBookmarksDeleteForm
 import com.freetalk.presenter.form.BoardDeleteForm
 import com.freetalk.presenter.form.BoardLikesDeleteForm
+import com.freetalk.presenter.form.BoardRelatedAllCommentMetaListSelectForm
+import com.freetalk.presenter.form.CommentDeleteForm
+import com.freetalk.presenter.form.CommentRelatedBookmarksDeleteForm
+import com.freetalk.presenter.form.CommentRelatedLikesDeleteForm
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import javax.inject.Inject
@@ -14,7 +19,8 @@ import javax.inject.Inject
 class DeleteBoardUseCase @Inject constructor(
     private val boardDataRepository: BoardDataRepository,
     private val bookmarkDataRepository: BookmarkDataRepository,
-    private val likeDataRepository: LikeDataRepository
+    private val likeDataRepository: LikeDataRepository,
+    private val commentDataRepository: CommentDataRepository
 ) {
     suspend operator fun invoke(
         boardDeleteForm: BoardDeleteForm,
@@ -22,6 +28,45 @@ class DeleteBoardUseCase @Inject constructor(
         boardLikesDeleteForm: BoardLikesDeleteForm,
         boardListEntity: BoardListEntity
     ): BoardListEntity = coroutineScope {
+
+        // TODO: 보드와 관련된 코멘트 다 가져옴 -> 코멘트 다 삭제 -> 코멘트와 연관된 라이크 삭제 -> 코멘트와 연관된 북마크 삭제
+
+        val boardRelatedCommentList = commentDataRepository.loadBoardRelatedAllCommentMetaList(boardRelatedAllCommentMetaListSelectForm = BoardRelatedAllCommentMetaListSelectForm(
+            boardAuthorEmail = boardDeleteForm.boardAuthorEmail,
+            boardCreateTime = boardDeleteForm.boardCreateTime
+        ))
+
+        boardRelatedCommentList.commentMetaList.map {
+            val commentDeleteAsync = async {
+                commentDataRepository.deleteComment(
+                    commentDeleteForm = CommentDeleteForm(
+                        commentAuthorEmail = it.author.email,
+                        commentCreateTime = it.createTime
+                    )
+                ) }
+            val commentLikesDeleteAsync = async {
+                likeDataRepository.deleteCommentRelatedLikes(
+                    commentRelatedLikesDeleteForm = CommentRelatedLikesDeleteForm(
+                        commentAuthorEmail = it.author.email,
+                        commentCreateTime = it.createTime
+                    )
+                )
+            }
+            val commentBookmarksAsync = async {
+                bookmarkDataRepository.deleteCommentRelatedBookmarks(
+                    commentRelatedBookmarksDeleteForm = CommentRelatedBookmarksDeleteForm(
+                        commentAuthorEmail = it.author.email,
+                        commentCreateTime = it.createTime
+                    )
+                )
+            }
+            Triple(commentDeleteAsync, commentLikesDeleteAsync, commentBookmarksAsync)
+        }.map { deferred ->
+            deferred.first.await()
+            deferred.second.await()
+            deferred.third.await()
+
+        }
 
         val asyncBoardDelete = async {
             boardDataRepository.deleteBoard(
@@ -39,6 +84,9 @@ class DeleteBoardUseCase @Inject constructor(
                 boardLikesDeleteForm = boardLikesDeleteForm
             )
         }
+
+
+
 
         asyncBoardDelete.await()
         asyncBookmarkDelete.await()
