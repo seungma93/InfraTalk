6f2ba16b6688dbcdf24291485b02889587faf089
seungma <seungma93@naver.com>
2023-11-30 20:21:55 +0900

    feature/mypage/base 내가 좋아요 누른 게시글 뷰까지 구현

---
+++ app/src/main/java/com/freetalk/presenter/adapter/MyLikeBoardListAdapter.kt
@@ -0,0 +1,123 @@
+package com.freetalk.presenter.adapter
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.freetalk.databinding.BoardListItemBinding
+import com.freetalk.databinding.ListItemMyBoardBinding
+import com.freetalk.databinding.ListItemMyBookmarkBoardBinding
+import com.freetalk.databinding.ListItemMyLikeBoardBinding
+import com.freetalk.domain.entity.BoardEntity
+import com.freetalk.domain.entity.BoardMetaEntity
+import com.freetalk.domain.entity.UserEntity
+
+
+class MyLikeBoardListAdapter(
+    private val itemClick: (BoardMetaEntity) -> Unit,
+    private val bookmarkClick: (BoardEntity) -> Unit,
+    private val likeClick: (BoardEntity) -> Unit,
+    private val deleteClick: (BoardEntity) -> Unit
+) : ListAdapter<BoardEntity, MyLikeBoardListAdapter.ViewHolder>(diffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            ListItemMyLikeBoardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding, itemClick, bookmarkClick, likeClick, deleteClick)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class ViewHolder(
+        private val binding: ListItemMyLikeBoardBinding,
+        private val itemClick: (BoardMetaEntity) -> Unit,
+        private val bookmarkClick: (BoardEntity) -> Unit,
+        private val likeClick: (BoardEntity) -> Unit,
+        private val deleteClick: (BoardEntity) -> Unit
+    ) :
+        RecyclerView.ViewHolder(binding.root) {
+        private var boardEntity: BoardEntity? = null
+
+        init {
+            binding.apply {
+                root.setOnClickListener {
+                    boardEntity?.let {
+                        Log.d("comment","리스트어댑터 데이터" + it.boardMetaEntity.author.email)
+                        itemClick(it.boardMetaEntity)
+                    }
+                }
+                btnBookmark.setOnClickListener {
+                    btnBookmark.isEnabled = false
+                    boardEntity?.let {
+                        bookmarkClick(it)
+                    }
+                }
+                btnLike.setOnClickListener {
+                    Log.d("board", "좋아요 람다 전달")
+                    btnLike.isEnabled = false
+                    boardEntity?.let {
+                        likeClick(it)
+                    }
+                }
+                btnDelete.setOnClickListener {
+                    boardEntity?.let {
+                        deleteClick(it)
+                    }
+                }
+            }
+
+        }
+
+        fun bind(boardEntity: BoardEntity) {
+            this.boardEntity = boardEntity
+            binding.apply {
+                boardEntity.let {
+                    Log.d("BoardListAdpater", "셀렉트 바인딩")
+                    title.text = it.boardMetaEntity.title
+                    context.text = it.boardMetaEntity.content
+                    date.text = it.boardMetaEntity.createTime.toString()
+                    author.text = it.boardMetaEntity.author.nickname
+                    btnBookmark.isSelected = it.bookmarkEntity.isBookmark
+                    btnLike.isSelected = it.likeEntity.isLike
+                    likeCount.text = it.likeCountEntity.likeCount.toString()
+                    /*
+                    Glide.with(itemView.context)
+                        .load(it.boardMetaEntity.images?.successUris?.firstOrNull())
+                        .into(ivSingleImage)
+
+                     */
+                    btnLike.isEnabled = true
+                    btnBookmark.isEnabled = true
+                }
+            }
+        }
+    }
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<BoardEntity>() {
+
+            // 두 아이템이 동일한 아이템인지 체크. 보통 고유한 id를 기준으로 비교
+            override fun areItemsTheSame(
+                oldItem: BoardEntity,
+                newItem: BoardEntity
+            ): Boolean {
+                return oldItem.boardMetaEntity.createTime == newItem.boardMetaEntity.createTime &&
+                        oldItem.boardMetaEntity.author == newItem.boardMetaEntity.author
+            }
+
+            // 두 아이템이 동일한 내용을 가지고 있는지 체크. areItemsTheSame()이 true일때 호출됨
+            override fun areContentsTheSame(
+                oldItem: BoardEntity,
+                newItem: BoardEntity
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/freetalk/presenter/fragment/mypage/MyLikeBoardFragment.kt
@@ -0,0 +1,213 @@
+package com.freetalk.presenter.fragment.mypage
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.freetalk.databinding.FragmentMyBookmarkBoardBinding
+import com.freetalk.databinding.FragmentMyLikeBoardBinding
+import com.freetalk.di.component.DaggerMyPageFragmentComponent
+import com.freetalk.domain.entity.BoardContentPrimaryKeyEntity
+import com.freetalk.domain.entity.UserEntity
+import com.freetalk.presenter.activity.EndPoint
+import com.freetalk.presenter.activity.Navigable
+import com.freetalk.presenter.adapter.MyBookmarkBoardListAdapter
+import com.freetalk.presenter.adapter.MyLikeBoardListAdapter
+import com.freetalk.presenter.form.BoardBookmarkAddForm
+import com.freetalk.presenter.form.BoardBookmarkDeleteForm
+import com.freetalk.presenter.form.BoardBookmarksDeleteForm
+import com.freetalk.presenter.form.BoardDeleteForm
+import com.freetalk.presenter.form.BoardLikeAddForm
+import com.freetalk.presenter.form.BoardLikeCountLoadForm
+import com.freetalk.presenter.form.BoardLikeDeleteForm
+import com.freetalk.presenter.form.BoardLikesDeleteForm
+import com.freetalk.presenter.viewmodel.MyBookmarkBoardViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class MyLikeBoardFragment: Fragment() {
+
+    private var _binding: FragmentMyLikeBoardBinding? = null
+    private val binding get() = _binding!!
+    private var _adapter: MyLikeBoardListAdapter? = null
+    private val adapter get() = _adapter!!
+
+    @Inject
+    lateinit var myLikeBoardViewModelFactory: ViewModelProvider.Factory
+    private val myLikeBoardViewModel: MyBookmarkBoardViewModel by viewModels { myLikeBoardViewModelFactory }
+
+    override fun onAttach(context: Context) {
+        //DaggerMyPageFragmentComponent.factory().create(context).inject(this)
+        super.onAttach(context)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMyLikeBoardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _adapter = MyLikeBoardListAdapter(
+            itemClick = {
+                Log.d("comment", "클릭시 넘어온 board값" + it.author.email)
+                val endPoint = EndPoint.BoardContent(
+                    boardContentPrimaryKeyEntity = BoardContentPrimaryKeyEntity(
+                        boardAuthorEmail = it.author.email,
+                        boardCreateTime = it.createTime
+                    )
+                )
+                (requireActivity() as? Navigable)?.navigateFragment(endPoint)
+            },
+            bookmarkClick = { boardEntity ->
+                boardEntity.apply {
+                    when (bookmarkEntity.isBookmark) {
+                        true -> {
+                            viewLifecycleOwner.lifecycleScope.launch {
+                                val boardViewState = myLikeBoardViewModel.deleteBookMark(
+                                    BoardBookmarkDeleteForm(
+                                        boardAuthorEmail = boardMetaEntity.author.email,
+                                        boardCreateTime = boardMetaEntity.createTime
+                                    )
+                                )
+                                adapter.submitList(boardViewState.boardListEntity.boardList)
+                            }
+                        }
+
+                        false -> {
+                            viewLifecycleOwner.lifecycleScope.launch {
+                                val boardViewState = myLikeBoardViewModel.addBookMark(
+                                    BoardBookmarkAddForm(
+                                        boardAuthorEmail = boardMetaEntity.author.email,
+                                        boardCreateTime = boardMetaEntity.createTime
+                                    )
+                                )
+                                adapter.submitList(boardViewState.boardListEntity.boardList)
+                            }
+                        }
+                    }
+                }
+            },
+            likeClick = { boardEntity ->
+                boardEntity.apply {
+                    when (likeEntity.isLike) {
+                        true -> {
+                            viewLifecycleOwner.lifecycleScope.launch {
+                                val boardViewState = myLikeBoardViewModel.deleteLike(
+                                    BoardLikeDeleteForm(
+                                        boardAuthorEmail = boardMetaEntity.author.email,
+                                        boardCreateTime = boardMetaEntity.createTime
+                                    ), BoardLikeCountLoadForm(
+                                        boardAuthorEmail = boardMetaEntity.author.email,
+                                        boardCreateTime = boardMetaEntity.createTime
+                                    )
+                                )
+                                adapter.submitList(boardViewState.boardListEntity.boardList)
+                            }
+                        }
+
+                        false -> {
+                            viewLifecycleOwner.lifecycleScope.launch {
+                                val boardViewState = myLikeBoardViewModel.addLike(
+                                    BoardLikeAddForm(
+                                        boardAuthorEmail = boardMetaEntity.author.email,
+                                        boardCreateTime = boardMetaEntity.createTime
+                                    ), BoardLikeCountLoadForm(
+                                        boardAuthorEmail = boardMetaEntity.author.email,
+                                        boardCreateTime = boardMetaEntity.createTime
+                                    )
+                                )
+                                adapter.submitList(boardViewState.boardListEntity.boardList)
+                            }
+                        }
+                    }
+                }
+            },
+            deleteClick = { boardEntity ->
+                showProgressBar()
+                boardEntity.apply {
+                    viewLifecycleOwner.lifecycleScope.launch {
+                        val myBoardViewState = myLikeBoardViewModel.deleteBoard(
+                            boardDeleteForm = BoardDeleteForm(
+                                boardAuthorEmail = boardMetaEntity.author.email,
+                                boardCreateTime = boardMetaEntity.createTime
+                            ),
+                            boardBookmarksDeleteForm = BoardBookmarksDeleteForm(
+                                boardAuthorEmail = boardMetaEntity.author.email,
+                                boardCreateTime = boardMetaEntity.createTime
+                            ),
+                            boardLikesDeleteForm = BoardLikesDeleteForm(
+                                boardAuthorEmail = boardMetaEntity.author.email,
+                                boardCreateTime = boardMetaEntity.createTime
+                            )
+                        )
+                        adapter.submitList(myBoardViewState.boardListEntity.boardList){
+                            hideProgressBar()
+                        }
+                    }
+                }
+            }
+        )
+        binding.apply {
+
+            swipeRefreshLayout.setOnRefreshListener {
+                viewLifecycleOwner.lifecycleScope.launch {
+                    kotlin.runCatching {
+                        myLikeBoardViewModel.loadMyBookmarkBoardList()
+                    }
+                    swipeRefreshLayout.isRefreshing = false
+                }
+
+            }
+
+            rvMyLikeBoardList.adapter = adapter
+        }
+        loadBoardList()
+    }
+
+    private fun loadBoardList() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            showProgressBar()
+            val boardViewState = myLikeBoardViewModel.loadMyBookmarkBoardList()
+            adapter.submitList(boardViewState.boardListEntity.boardList) {
+                binding.rvMyLikeBoardList.scrollToPosition(0)
+                hideProgressBar()
+            }
+
+        }
+    }
+    private fun showProgressBar() {
+        Log.d("BoardFragment", "프로그레스바 시작")
+        blockLayoutTouch()
+        binding.progressBar.isVisible = true
+    }
+
+    private fun blockLayoutTouch() {
+        requireActivity().window?.setFlags(
+            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+        )
+    }
+
+    private fun hideProgressBar() {
+        Log.d("BoardFragment", "프로그레스바 종료")
+        clearBlockLayoutTouch()
+        binding.progressBar.isVisible = false
+    }
+
+    private fun clearBlockLayoutTouch() {
+        requireActivity().window?.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+    }
+}
