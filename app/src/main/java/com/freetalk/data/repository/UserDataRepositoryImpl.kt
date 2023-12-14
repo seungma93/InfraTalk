eeeaacfbda4a0a8b28331f841199335cdcaf9ca0
seungma <seungma93@naver.com>
2023-12-14 20:24:16 +0900

    feature/mypage/base 마이 페이지 정보 변경 유저 데이터소스 수정으로 인한 기존 sign뷰모델 수정

--- app/src/main/java/com/freetalk/data/repository/UserDataRepositoryImpl.kt
+++ app/src/main/java/com/freetalk/data/repository/UserDataRepositoryImpl.kt
@@ -2,15 +2,16 @@ package com.freetalk.domain.repository
 
 import android.util.Log
 import com.freetalk.data.datasource.remote.UserDataSource
+import com.freetalk.data.model.request.UserInfoUpdateRequest
 import com.freetalk.domain.entity.UserEntity
 import com.freetalk.presenter.form.LogInForm
 import com.freetalk.presenter.form.ResetPasswordForm
 import com.freetalk.presenter.form.SignUpForm
-import com.freetalk.presenter.form.UpdateForm
+import com.freetalk.presenter.form.UserInfoUpdateForm
 import toEntity
 import javax.inject.Inject
 
-class UserDataRepositoryImpl @Inject constructor(private val dataSource: UserDataSource):
+class UserDataRepositoryImpl @Inject constructor(private val dataSource: UserDataSource) :
     UserDataRepository {
 
     override suspend fun signUp(signUpForm: SignUpForm): UserEntity {
@@ -32,9 +33,16 @@ class UserDataRepositoryImpl @Inject constructor(private val dataSource: UserDat
         return dataSource.sendVerifiedEmail().toEntity()
     }
 
-    override suspend fun updateUserInfo(updateForm: UpdateForm): UserEntity {
-        return dataSource.updateUserInfo(updateForm).toEntity()
-    }
+    override suspend fun updateUserInfo(userInfoUpdateForm: UserInfoUpdateForm): UserEntity =
+        with(userInfoUpdateForm) {
+            return dataSource.updateUserInfo(
+                userInfoUpdateRequest = UserInfoUpdateRequest(
+                    email = email,
+                    nickname = nickname,
+                    image = image
+                )
+            ).toEntity()
+        }
 
     override suspend fun deleteUserInfo(signUpForm: SignUpForm): UserEntity {
         return dataSource.deleteUserInfo(signUpForm).toEntity()
--- app/src/main/java/com/freetalk/domain/repository/UserDataRepository.kt
+++ app/src/main/java/com/freetalk/domain/repository/UserDataRepository.kt
@@ -4,14 +4,14 @@ import com.freetalk.domain.entity.UserEntity
 import com.freetalk.presenter.form.LogInForm
 import com.freetalk.presenter.form.ResetPasswordForm
 import com.freetalk.presenter.form.SignUpForm
-import com.freetalk.presenter.form.UpdateForm
+import com.freetalk.presenter.form.UserInfoUpdateForm
 
 interface UserDataRepository {
     suspend fun signUp(signUpForm: SignUpForm): UserEntity
     suspend fun logIn(logInForm: LogInForm): UserEntity
     suspend fun resetPassword(resetPasswordForm: ResetPasswordForm): UserEntity
     suspend fun sendVerifiedEmail(): UserEntity
-    suspend fun updateUserInfo(updateForm: UpdateForm): UserEntity
+    suspend fun updateUserInfo(userInfoUpdateForm: UserInfoUpdateForm): UserEntity
     suspend fun deleteUserInfo(signUpForm: SignUpForm): UserEntity
     fun getUserInfo(): UserEntity
 }
