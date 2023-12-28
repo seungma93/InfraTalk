d27f858b5082c55139a4d9b54cbd92f7121d58ca
seungma <seungma93@naver.com>
2023-12-28 13:41:00 +0900

    feature/board/base DeleteBoardContentBookmarkUseCaseTest 작성

---
+++ app/src/test/java/com/freetalk/domain/usecase/DeleteBoardContentBookmarkUseCaseTest.kt
@@ -0,0 +1,54 @@
+package com.freetalk.domain.usecase
+
+import com.freetalk.domain.entity.BoardEntity
+import com.freetalk.domain.entity.BookmarkEntity
+import com.freetalk.domain.repository.BookmarkDataRepository
+import com.freetalk.presenter.form.BoardBookmarkDeleteForm
+import io.mockk.MockK
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+import java.util.Date
+
+class DeleteBoardContentBookmarkUseCaseTest {
+    private val repository: BookmarkDataRepository = mockk()
+    private lateinit var useCase: DeleteBoardContentBookmarkUseCase
+
+    @Before
+    fun setUp() {
+        useCase = DeleteBoardContentBookmarkUseCase(repository)
+    }
+
+    @Test
+    fun `게시글 콘텐츠 북마크 삭제`() = runTest {
+        //given
+        val currentTime = System.currentTimeMillis()
+        val boardBookmarkDeleteForm: BoardBookmarkDeleteForm = mockk(relaxed = true) {
+            every { boardAuthorEmail } returns "email"
+            every { boardCreateTime } returns Date(currentTime)
+        }
+        val boardEntity: BoardEntity = mockk(relaxed = true) {
+            every { bookmarkEntity.isBookmark } returns true
+            every { boardMetaEntity } returns mockk(relaxed = true) {
+                every { author.email } returns "email"
+                every { createTime } returns Date(currentTime)
+            }
+
+        }
+
+        coEvery { repository.deleteBoardBookmark(any()) } returns BookmarkEntity(isBookmark = false)
+
+        //when
+        val result = useCase.invoke(boardBookmarkDeleteForm, boardEntity)
+
+        //then
+        assertFalse(result.bookmarkEntity.isBookmark)
+
+        coVerify(exactly = 1) {repository.deleteBoardBookmark(any()) }
+    }
+}
