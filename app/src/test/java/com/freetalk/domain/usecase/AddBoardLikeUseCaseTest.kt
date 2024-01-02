000185c3cd4b1f24f86fb33a9e24560fc51b4aa8
seungma <seungma93@naver.com>
2024-01-02 17:57:02 +0900

    feature/board/base AddBoardLikeUseCaseTest 작성

---
+++ app/src/test/java/com/freetalk/domain/usecase/AddBoardLikeUseCaseTest.kt
@@ -0,0 +1,58 @@
+package com.freetalk.domain.usecase
+
+import com.freetalk.domain.entity.BoardListEntity
+import com.freetalk.domain.entity.LikeEntity
+import com.freetalk.domain.repository.LikeDataRepository
+import com.freetalk.presenter.form.BoardLikeAddForm
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+import java.util.Date
+
+class AddBoardLikeUseCaseTest {
+    private val repository: LikeDataRepository = mockk()
+    private lateinit var useCase: AddBoardLikeUseCase
+
+    @Before
+    fun setUp() {
+        useCase = AddBoardLikeUseCase(repository)
+    }
+
+    @Test
+    fun `게시글 좋아요 추가 테스트`() = runTest {
+        //given
+        val currentTime = System.currentTimeMillis()
+        val boardLikeAddForm: BoardLikeAddForm = mockk(relaxed = true) {
+            every { boardAuthorEmail } returns "email"
+            every { boardCreateTime } returns Date(currentTime)
+        }
+        val entity = BoardListEntity(
+            boardList = listOf(
+                mockk(relaxed = true) {
+                    every { likeEntity.isLike } returns false
+                    every { boardMetaEntity } returns mockk(relaxed = true) {
+                        every { author.email } returns "email"
+                        every { createTime } returns Date(currentTime)
+                    }
+                }
+            )
+        )
+
+        coEvery { repository.addBoardLike(any()) } returns LikeEntity(isLike = true)
+        coEvery { repository.loadBoardLikeCount(any()) } returns mockk()
+
+        //when
+        val result = useCase.invoke(boardLikeAddForm, mockk(), entity)
+
+        //then
+        assertTrue(result.boardList[0].likeEntity.isLike)
+
+        coVerify(exactly = 1) { repository.addBoardLike(any()) }
+        coVerify(exactly = 1) { repository.loadBoardLikeCount(any()) }
+    }
+}
