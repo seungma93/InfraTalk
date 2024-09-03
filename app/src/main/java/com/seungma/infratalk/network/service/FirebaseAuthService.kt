7f748a374bce19aea75e88812953cddaba0519c4
seungma93 <seungma93@naver.com>
2024-09-03 19:35:36 +0900

    Refactor: firebaseAuth REST API 테스트 (404까지 확인)

--- app/src/main/java/com/seungma/infratalk/network/service/FirebaseAuthService.kt
+++ app/src/main/java/com/seungma/infratalk/network/service/FirebaseAuthService.kt
@@ -4,13 +4,15 @@ import com.seungma.infratalk.data.model.request.FirebaseIdTokenRequest
 import com.seungma.infratalk.data.model.response.UserEmailResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 
 interface FirebaseAuthService {
-        @POST("accounts:lookup")
-        suspend fun getUserInfo(
-                @Body request: FirebaseIdTokenRequest
-        ): UserEmailResponse
+    @POST("/accounts:lookup")
+    suspend fun getUserInfo(
+        @Query("key") apiKey: String,
+        @Body request: FirebaseIdTokenRequest
+    ): UserEmailResponse
 }
 
 
