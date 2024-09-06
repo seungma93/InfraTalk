6173d7916f562ac5f52d5b1308700f06fa3be052
seungma93 <seungma93@naver.com>
2024-09-06 11:46:39 +0900

    Refactor: 자동 로그인 구현 완 (테스트 필요)

--- app/src/main/java/com/seungma/infratalk/presenter/main/activity/MainActivity.kt
+++ app/src/main/java/com/seungma/infratalk/presenter/main/activity/MainActivity.kt
@@ -31,11 +31,18 @@ interface Navigable {
 class MainActivity() : AppCompatActivity(), Navigable {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
+    private var loginSuccessKey: Boolean = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        loginSuccessKey = intent.getBooleanExtra("loginSuccessKey", false)
+        when(loginSuccessKey) {
+            true -> navigateFragment(EndPoint.Main)
+            false -> navigateFragment(EndPoint.LoginMain)
+        }
         navigateFragment(EndPoint.LoginMain)
     }
 
