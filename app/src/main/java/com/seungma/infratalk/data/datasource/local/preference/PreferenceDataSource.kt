53b5f88be20483de2dec191241ea8ff924b65aa2
seungma93 <seungma93@naver.com>
2024-09-30 21:18:49 +0900

    Feat: 프리퍼런스 데이터소스 구현(getSavedEmail)

--- app/src/main/java/com/seungma/infratalk/data/datasource/local/preference/PreferenceDataSource.kt
+++ app/src/main/java/com/seungma/infratalk/data/datasource/local/preference/PreferenceDataSource.kt
@@ -1,6 +1,7 @@
 package com.seungma.infratalk.data.datasource.local.preference
 
 import com.seungma.infratalk.data.model.request.preference.UserTokenSetRequest
+import com.seungma.infratalk.data.model.response.preference.SavedEmailResponse
 import com.seungma.infratalk.data.model.response.preference.UserTokenResponse
 
 
@@ -8,5 +9,6 @@ interface PreferenceDataSource {
     fun getUserToken(): UserTokenResponse
     fun setUserToken(userTokenSetRequest: UserTokenSetRequest)
     fun deleteUserToken()
+    fun getSavedEmail(): SavedEmailResponse
 }
 
