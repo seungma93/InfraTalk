196cec9205eb47826477299f7229d955885b2723
seungma93 <seungma93@naver.com>
2024-12-18 19:05:49 +0900

    Refactor: 게시글 사진 다이어로그 생성

---
+++ app/src/main/java/com/seungma/infratalk/presenter/board/fragment/DialogImageFragment.kt
@@ -0,0 +1,39 @@
+package com.seungma.infratalk.presenter.board.fragment
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.DialogFragment
+import com.seungma.infratalk.databinding.DialogBoardImageBinding
+
+class DialogImageFragment : DialogFragment() {
+    private var _binding: DialogBoardImageBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DialogBoardImageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+    }
+
+
+}
