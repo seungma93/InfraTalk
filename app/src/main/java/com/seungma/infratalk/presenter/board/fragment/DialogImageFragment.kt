a7abc59a381913d9c14d29e075111d059f578f5d
seungma93 <seungma93@naver.com>
2024-12-20 20:48:01 +0900

    Refactor: 게시판 이미지 리사이클러 뷰로 변경

--- app/src/main/java/com/seungma/infratalk/presenter/board/fragment/DialogImageFragment.kt
+++ app/src/main/java/com/seungma/infratalk/presenter/board/fragment/DialogImageFragment.kt
@@ -1,21 +1,14 @@
 package com.seungma.infratalk.presenter.board.fragment
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.util.DisplayMetrics
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.fragment.app.DialogFragment
-import com.bumptech.glide.Glide
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.seungma.infratalk.databinding.DialogBoardImageBinding
 import com.seungma.infratalk.domain.image.entity.ImagesResultEntity
+import com.seungma.infratalk.presenter.board.adpater.ImageAdapter
 
 class DialogImageFragment(private val imagesResultEntity: ImagesResultEntity?) : DialogFragment() {
     private var _binding: DialogBoardImageBinding? = null
@@ -31,15 +24,11 @@ class DialogImageFragment(private val imagesResultEntity: ImagesResultEntity?) :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        imagesResultEntity?.successUris?.map {
-            Log.d("seungma", "이미지 : " + it)
-            val imageView = ImageView(requireContext())
-            Glide.with(imageView.context)
-                .load(it)
-                .centerCrop()
-                .into(imageView)
-            binding.rvImage.addView(imageView)
-        }
+
+        val recyclerView = binding.rvImage // RecyclerView
+        val layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.layoutManager= layoutManager
+        recyclerView.adapter = ImageAdapter(imagesResultEntity?.successUris ?: emptyList()) // Adapter 설정
     }
 
     override fun onStart() {
