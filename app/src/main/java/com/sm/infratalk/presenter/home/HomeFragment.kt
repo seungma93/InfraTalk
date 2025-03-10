305a55ed3f733233797a05bbc2b9857df80424d9
seungma93 <seungma93@naver.com>
2025-03-11 08:32:30 +0900

    Refactor: 홈 화면 문구 오타 수정

--- app/src/main/java/com/sm/infratalk/presenter/home/HomeFragment.kt
+++ app/src/main/java/com/sm/infratalk/presenter/home/HomeFragment.kt
@@ -30,7 +30,7 @@ class HomeFragment : Fragment() {
             tvDeveloperInfo.text =
                 "[infratalk@home] uname -an \n[infratalk@home] 안드로이드 개발자 전직 미들웨어 엔지니어\n" +
                         "[infratalk@home] email \n[infratalk@home] seungma93@naver.com " +
-                        "[infratalk@home] help \n[infratalk@home]  \"앱 사용시 문제가 있으면 이메일로 문의 주세요"
+                        "[infratalk@home] help \n[infratalk@home]  앱 사용시 문제가 있으면 이메일로 문의 주세요"
 
 
             lyOracle.setOnClickListener {
