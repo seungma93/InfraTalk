017e076750f2c0780d69adc0af22144ec62b6efc
seungma93 <seungma93@naver.com>
2025-04-30 14:22:39 +0900

    Refactor: 머테리얼 디펜던시 추가 카드뷰 에러 수정

--- app/src/main/java/com/sm/infratalk/presenter/common/CustomSnackbarComponent.kt
+++ app/src/main/java/com/sm/infratalk/presenter/common/CustomSnackbarComponent.kt
@@ -6,8 +6,7 @@ import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -48,11 +47,10 @@ fun CustomSnackbar(
             enter = slideInVertically(initialOffsetY = { it }),
             exit = slideOutVertically(targetOffsetY = { it })
         ) {
-            Card(
+            ElevatedCard(
                 modifier = Modifier
                     .padding(16.dp)
                     .fillMaxWidth(),
-                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
             ) {
                 Text(
                     text = message,
