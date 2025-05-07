8ac5842048210b514a181758e3897ca5819f7cfe
seungma93 <seungma93@naver.com>
2025-05-07 13:20:07 +0900

    Refactor: 회원가입 스낵바 호출 재수정

--- app/src/main/java/com/sm/infratalk/presenter/sign/fragment/SignUpFragment.kt
+++ app/src/main/java/com/sm/infratalk/presenter/sign/fragment/SignUpFragment.kt
@@ -73,33 +73,58 @@ class SignUpFragment : Fragment() {
                 SignUpScreen(
                     isLoading = false,
                     onSignUpClick = { email, password, passwordCheck, nickname ->
-                        if (password == passwordCheck) {
-                            viewLifecycleOwner.lifecycleScope.launch {
-                                selectedImageUri?.let {
-                                    signViewModel.signUp(
-                                        signUpForm = SignUpForm(
-                                            email = email,
-                                            password = password,
-                                            nickname = nickname
-                                        ),
-                                        imagesRequest = ImagesRequest(
-                                            imageUris = listOf(it)
-                                        )
-                                    )
-                                } ?: run {
-                                    signViewModel.signUp(
-                                        signUpForm = SignUpForm(
-                                            email = email,
-                                            password = password,
-                                            nickname = nickname
-                                        ),
-                                        imagesRequest = null
-                                    )
+
+                        when {
+                            email.isEmpty() -> {
+                                snackbarMessage = "이메일을 입력해 주세요"
+                                showSnackbar = true
+                            }
+
+                            password.isEmpty() -> {
+                                snackbarMessage = "비밀번호를 입력해 주세요"
+                                showSnackbar = true
+                            }
+
+                            passwordCheck.isEmpty() -> {
+                                snackbarMessage = "비밀번호 확인을 입력해 주세요"
+                                showSnackbar = true
+                            }
+
+                            nickname.isEmpty() -> {
+                                snackbarMessage = "비밀번호 확인을 입력해 주세요"
+                                showSnackbar = true
+                            }
+
+                            else -> {
+                                if (password == passwordCheck) {
+                                    viewLifecycleOwner.lifecycleScope.launch {
+                                        selectedImageUri?.let {
+                                            signViewModel.signUp(
+                                                signUpForm = SignUpForm(
+                                                    email = email,
+                                                    password = password,
+                                                    nickname = nickname
+                                                ),
+                                                imagesRequest = ImagesRequest(
+                                                    imageUris = listOf(it)
+                                                )
+                                            )
+                                        } ?: run {
+                                            signViewModel.signUp(
+                                                signUpForm = SignUpForm(
+                                                    email = email,
+                                                    password = password,
+                                                    nickname = nickname
+                                                ),
+                                                imagesRequest = null
+                                            )
+                                        }
+                                    }
+                                } else {
+                                    snackbarMessage = "비밀번호가 일치하지 않습니다"
+                                    showSnackbar = true
                                 }
                             }
-                        } else {
-                            snackbarMessage = "비밀번호가 일치하지 않습니다"
-                            showSnackbar = true
                         }
                     },
                     onProfileImageClick = {
