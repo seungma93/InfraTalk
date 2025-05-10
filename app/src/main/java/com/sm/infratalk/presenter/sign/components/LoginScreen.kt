cfad331c0a8cf1d6fa0ef02e8301dcf207348910
seungma93 <seungma93@naver.com>
2025-05-10 17:35:40 +0900

    Refactor: 로그인 컴포즈 컬러 수정

--- app/src/main/java/com/sm/infratalk/presenter/sign/components/LoginScreen.kt
+++ app/src/main/java/com/sm/infratalk/presenter/sign/components/LoginScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -33,6 +34,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -117,6 +119,8 @@ fun LoginScreen(
         }
     }
 
+    val colorPrimaryDark = colorResource(id = R.color.colorPrimaryDark)
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
@@ -146,7 +150,7 @@ fun LoginScreen(
                             fontSize = 50.sp
                         ),
                         fontWeight = FontWeight.Bold,
-                        color = MaterialTheme.colorScheme.primary,
+                        color = colorPrimaryDark,
                         modifier = Modifier.padding(top = 10.dp)
                     )
                 }
@@ -196,7 +200,8 @@ fun LoginScreen(
                     },
                     modifier = Modifier
                         .width(250.dp)
-                        .height(65.dp)
+                        .height(65.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = colorPrimaryDark)
                 ) {
                     Text(
                         "로그인",
