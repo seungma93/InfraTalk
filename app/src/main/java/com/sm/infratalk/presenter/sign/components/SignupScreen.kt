ea1caff17285e4454e86234b14eed06f9689188e
seungma93 <seungma93@naver.com>
2025-06-03 18:28:16 +0900

    Refactor: 회원가입 버튼 라운드 수정

--- app/src/main/java/com/sm/infratalk/presenter/sign/components/SignupScreen.kt
+++ app/src/main/java/com/sm/infratalk/presenter/sign/components/SignupScreen.kt
@@ -1,3 +1,4 @@
+
 import android.net.Uri
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -10,8 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -27,7 +27,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -163,7 +162,8 @@ fun SignUpScreen(
                     .height(60.dp),
                 colors = ButtonDefaults.buttonColors(
                     containerColor = colorResource(id = R.color.colorAccent)
-                )
+                ),
+                shape = RoundedCornerShape(10)
             ) {
                 Text(
                     "회원가입",
