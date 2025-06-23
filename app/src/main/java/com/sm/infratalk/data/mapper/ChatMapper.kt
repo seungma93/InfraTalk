27bcb7b6143667b7f11fbd9bcecb52ac1b4bbbee
seungma93 <seungma93@naver.com>
2025-06-23 16:24:09 +0900

    Feat: ChatMessageNotifyResponse.toEntity 생성

--- app/src/main/java/com/sm/infratalk/data/mapper/ChatMapper.kt
+++ app/src/main/java/com/sm/infratalk/data/mapper/ChatMapper.kt
@@ -1,6 +1,7 @@
 package com.sm.infratalk.data.mapper
 
 import com.sm.infratalk.data.model.response.chat.ChatMessageListResponse
+import com.sm.infratalk.data.model.response.chat.ChatMessageNotifyResponse
 import com.sm.infratalk.data.model.response.chat.ChatMessageResponse
 import com.sm.infratalk.data.model.response.chat.ChatMessageSendResponse
 import com.sm.infratalk.data.model.response.chat.ChatRoomCheckResponse
@@ -95,5 +96,14 @@ fun LastChatMessageResponse.toEntity(): LastChatMessageEntity {
 fun ChatRoomLeaveResponse.toEntity(): ChatRoomLeaveEntity {
     return ChatRoomLeaveEntity(
         isSuccess = isSuccess ?: false
+    )
+}
+
+fun ChatMessageNotifyResponse.toEntity(): com.sm.infratalk.domain.chat.entity.ChatMessageNotifyEntity {
+    return com.sm.infratalk.domain.chat.entity.ChatMessageNotifyEntity(
+        roomId = roomId.orEmpty(),
+        sender = sender.orEmpty(),
+        content = content.orEmpty(),
+        sendTimestamp = sendTimestamp ?: java.util.Date()
     )
 }
