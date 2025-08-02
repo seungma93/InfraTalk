737bea06407bf23bc04ffec6b5ddb8c7588f4f6c
seungma93 <seungma93@naver.com>
2025-08-02 20:41:38 +0900

    Refactor: 활성화 채팅이 아닐때 노티 발생

--- app/src/main/java/com/sm/infratalk/presenter/service/ForegroundService.kt
+++ app/src/main/java/com/sm/infratalk/presenter/service/ForegroundService.kt
@@ -153,10 +153,12 @@ class ForegroundService : Service(), ViewModelStoreOwner {
             Log.d("seungma", "subscribe collect 시작")
             serviceViewModel.chatNotification.collect { chatMessage ->
                 Log.d("seungma", "채팅 들어옴: $chatMessage")
-                
-                // 채팅 메시지가 null이 아닐 때만 노티 보내기
+
+
+
+                // 채팅 메시지가 null이 아니고, 채팅방 활성이 아닐때 노티 밣생
                 chatMessage?.let { message ->
-                    updateNotification(message)
+                    if(!ActiveChatTracker.isActiveChatWith(message.roomId)) updateNotification(message)
                 }
             }
         }
