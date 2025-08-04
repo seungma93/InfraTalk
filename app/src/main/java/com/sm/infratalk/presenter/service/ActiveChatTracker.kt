5c212ad0d7f092a056b0fff84b1fe87203ab5eaf
seungma93 <seungma93@naver.com>
2025-08-04 12:25:34 +0900

    Refactor: ActiveChatTracker 파일 이름 변경

--- app/src/main/java/com/sm/infratalk/presenter/service/ActiveChatTracker.kt
+++ app/src/main/java/com/sm/infratalk/presenter/service/ActiveChatTracker.kt
@@ -1,16 +1,6 @@
 package com.sm.infratalk.presenter.service
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.os.Build
 import android.util.Log
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import com.sm.infratalk.R
-import com.sm.infratalk.presenter.main.activity.MainActivity
 
 // 현재 활성화된 채팅방 ID를 추적하는 object
 object ActiveChatTracker {
