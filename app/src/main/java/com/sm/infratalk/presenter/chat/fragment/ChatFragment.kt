d5141f64cbf5f88278d102d4f4bb948a2984abeb
seungma93 <seungma93@naver.com>
2025-08-10 21:27:36 +0900

    Refactor: setActiveChat 파라미터 수정

--- app/src/main/java/com/sm/infratalk/presenter/chat/fragment/ChatFragment.kt
+++ app/src/main/java/com/sm/infratalk/presenter/chat/fragment/ChatFragment.kt
@@ -15,6 +15,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.test.core.app.ActivityScenario.launch
 import com.google.android.material.snackbar.Snackbar
 import com.sm.infratalk.databinding.FragmentChatBinding
 import com.sm.infratalk.di.component.DaggerChatFragmentComponent
@@ -196,7 +197,7 @@ class ChatFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
-        ActiveChatTracker.setActiveChat(chatId = chatPrimaryKeyEntity.chatRoomId)
+        ActiveChatTracker.setActiveChat(chatId = chatPrimaryKeyEntity.chatRoomName)
 
     }
 
