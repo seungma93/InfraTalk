7342d16e8aaec8652146d67f288e602b62d1f728
seungma93 <seungma93@naver.com>
2025-08-31 11:41:05 +0900

    Feat: 리스트 아이템 생성

--- app/src/main/java/com/sm/infratalk/presenter/board/components/BoardScreen.kt
+++ app/src/main/java/com/sm/infratalk/presenter/board/components/BoardScreen.kt
@@ -1,7 +1,13 @@
 package com.sm.infratalk.presenter.board.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.sm.infratalk.domain.board.entity.BoardEntity
 
 
@@ -18,5 +24,13 @@ fun BoardItemList(
     }
 }
 
+// 아이템
+@Composable
+fun BoardItemRow(item: BoardEntity, onClick: () -> Unit) {
+    Row(
+        modifier = Modifier.fillMaxWidth().clickable(onClick = onClick).padding(16.dp)
+    ) {
 
+    }
+}
 
