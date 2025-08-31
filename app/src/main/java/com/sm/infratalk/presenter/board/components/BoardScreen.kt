bc4f228198a6611ca1371ef7de31f18d591a3096
seungma93 <seungma93@naver.com>
2025-09-01 07:55:55 +0900

    Feat: 게시글 제목, 내용 생성

--- app/src/main/java/com/sm/infratalk/presenter/board/components/BoardScreen.kt
+++ app/src/main/java/com/sm/infratalk/presenter/board/components/BoardScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -30,7 +32,12 @@ fun BoardItemRow(item: BoardEntity, onClick: () -> Unit) {
     Row(
         modifier = Modifier.fillMaxWidth().clickable(onClick = onClick).padding(16.dp)
     ) {
+        // TODO("작성자, 날짜")
 
+        // TODO("제목, 내용")
+            Text(modifier = Modifier.size(20.dp), text = item.boardMetaEntity.title)
+            Text(modifier = Modifier.size(20.dp), text = item.boardMetaEntity.content)
+        // TODO("버튼")
     }
 }
 
