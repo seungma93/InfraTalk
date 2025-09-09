6a1343a96ed9c44159c1ba799ed6eb186790c1c8
seungma93 <seungma93@naver.com>
2025-09-10 07:45:56 +0900

    Feat: 게시글 로드 호출, BoardItemList 호출

--- app/src/main/java/com/sm/infratalk/presenter/board/components/BoardScreen.kt
+++ app/src/main/java/com/sm/infratalk/presenter/board/components/BoardScreen.kt
@@ -1,14 +1,18 @@
 package com.sm.infratalk.presenter.board.components
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -16,6 +20,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -31,9 +36,9 @@ import com.sm.infratalk.domain.board.entity.BookmarkEntity
 import com.sm.infratalk.domain.board.entity.LikeCountEntity
 import com.sm.infratalk.domain.board.entity.LikeEntity
 import com.sm.infratalk.domain.user.entity.UserEntity
+import com.sm.infratalk.presenter.board.form.BoardListLoadForm
 import com.sm.infratalk.presenter.board.viewmodel.BoardViewModel
 import java.util.Date
-
 
 
 @Composable
@@ -46,7 +51,29 @@ fun BoardScreen(
 
     LaunchedEffect(Unit) {
         isLoading = true
+        try {
+            val result = viewModel.loadBoardList(BoardListLoadForm(reload = true))
+            boardItems = result.boardListEntity.boardList
+        } catch (e: Exception) {
+            // 에러 처리
+            Log.d("BoardScreen", "게시글 로드 실패", e)
+        } finally {
+            isLoading = false
+        }
+    }
 
+    if (isLoading) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            CircularProgressIndicator()
+        }
+
+    } else {
+        BoardItemList(
+            items = boardItems,
+            onItemClick = {},
+            onBookmarkClick = {},
+            onLikeClick = {}, onChatClick = {}, onRemove = {}, onLoadMore = {}
+        )
     }
 }
 
