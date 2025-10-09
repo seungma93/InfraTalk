f2475d1f63169b3ffa9f6f2088dbb7cd83771124
seungma93 <seungma93@naver.com>
2025-10-10 07:46:15 +0900

    Refactor: 이미지 리스트 LazyRow로 수정

--- app/src/main/java/com/sm/infratalk/presenter/board/components/BoardContentScreen.kt
+++ app/src/main/java/com/sm/infratalk/presenter/board/components/BoardContentScreen.kt
@@ -6,11 +6,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.CircularProgressIndicator
@@ -245,7 +248,6 @@ fun BoardContent(
     onLikeClick: (BoardEntity) -> Unit,
     onChatClick: (BoardEntity) -> Unit
 ) {
-    val listState = rememberLazyListState()
 
     Column(
         modifier = Modifier
@@ -286,8 +288,12 @@ fun BoardContent(
 
         // TODO("제목, 내용")
         Text(modifier = Modifier.fillMaxWidth(), text = item.boardMetaEntity.title)
-        LazyColumn(
-            state = listState  // 스크롤 상태 연결
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp), // 리스트 자체의 높이
+            horizontalArrangement = Arrangement.spacedBy(8.dp), // 아이템 간격
+            contentPadding = PaddingValues(horizontal = 16.dp)
         ) {
             item.boardMetaEntity.images?.successUris?.let { items ->
                 items(items = items) { item ->
@@ -487,3 +493,72 @@ fun BoardCommentItemRow(
         }
     }
 }
+/*
+
+@Composable
+fun ImageItemRow(
+    item: CommentEntity
+) {
+    Column {
+        Row {
+            Text(modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp),
+                text = item.commentMetaEntity.author.nickname)
+            if(userEntity.email == item.commentMetaEntity.author.email){
+                Image(
+                    painter = painterResource(
+                        id = R.drawable.ic_clear
+                    ),
+                    contentDescription = stringResource(R.string.app_name),
+                    modifier = Modifier.clickable {
+                        onBookmarkClick(item)
+                    }, // 크기, 패딩 등 지정 가능
+                    contentScale = ContentScale.Crop // 이미지 크기 조절 방식
+                )
+            }
+        }
+
+        Text(modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 10.dp),
+            text = item.commentMetaEntity.content)
+        Row {
+            Text(modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 10.dp),
+                text = item.commentMetaEntity.createTime.toString())
+            Image(
+                painter = painterResource(
+                    id = if (item.bookmarkEntity.isBookmark) {
+                        R.drawable.btn_star_pressed  // 북마크된 상태
+                    } else {
+                        R.drawable.btn_star_default   // 북마크 안된 상태
+                    }
+                ),
+                contentDescription = stringResource(R.string.app_name),
+                modifier = Modifier.clickable {
+                    onBookmarkClick(item)
+                }, // 크기, 패딩 등 지정 가능
+                contentScale = ContentScale.Crop // 이미지 크기 조절 방식
+            )
+            Image(
+                painter = painterResource(
+                    id = if (item.likeEntity.isLike) {
+                        R.drawable.btn_like_pressed  // 좋아요된 상태
+                    } else {
+                        R.drawable.btn_like_default   // 좋아요 안된 상태
+                    }
+                ),
+                contentDescription = stringResource(R.string.app_name),
+                modifier = Modifier.clickable {
+                    onLikeClick(item)
+                }, // 크기, 패딩 등 지정 가능
+                contentScale = ContentScale.Crop // 이미지 크기 조절 방식
+            )
+            Text(modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp),
+                text = item.likeCountEntity.likeCount.toString()
+            )
+        }
+    }
+}*/
