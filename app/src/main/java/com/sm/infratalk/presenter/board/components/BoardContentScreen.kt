60a0191bc5e718c46552005adc5d08cbb8cc29f8
seungma93 <seungma93@naver.com>
2025-10-15 08:35:45 +0900

    Refactor: 다이어로그 팝업 구현

--- app/src/main/java/com/sm/infratalk/presenter/board/components/BoardContentScreen.kt
+++ app/src/main/java/com/sm/infratalk/presenter/board/components/BoardContentScreen.kt
@@ -3,6 +3,7 @@ package com.sm.infratalk.presenter.board.components
 import android.net.Uri
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,11 +15,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -30,10 +35,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.rememberAsyncImagePainter
 import com.sm.infratalk.R
@@ -315,7 +322,14 @@ fun BoardContent(
         }
 
         if(showDialog) {
-            // 다이어로그 호출
+
+            item.boardMetaEntity.images?.successUris?.let {
+                // 다이어로그 호출
+                ImageSliderDialog(images = it, initialIndex = 0, onDismiss = {
+                    showDialog = false
+                })
+            }
+
         }
 
         Text(modifier = Modifier.fillMaxWidth(), text = item.boardMetaEntity.content)
